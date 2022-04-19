46b3b352adb818a9e4ff142348575a082ee9fba8
Dayo Adebanjo <dahyor@outlook.com>
2022-04-19 20:12:07 +0100

    switched rabbitmq

--- settings.py
+++ settings.py
@@ -2,11 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-RABBITMQ_USER = os.environ.get('RABBITMQ_DEFAULT_USER');
-RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_DEFAULT_PASS');
-RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST');
-RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT');
 REDIS_HOST = os.environ.get('REDIS_HOST');
 REDIS_PORT = os.environ.get('REDIS_PORT');
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD');
@@ -16,7 +11,7 @@ S3_UPLOAD_KEY = os.environ.get('S3_UPLOAD_KEY')
 S3_UPLOAD_SECRET = os.environ.get('S3_UPLOAD_SECRET')
 S3_UPLOAD_BUCKET = os.environ.get('S3_UPLOAD_BUCKET')
 
-AMPQ_URL = f'{RABBITMQ_PROTOCOOL}://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}'
+AMPQ_URL = os.environ.get('RABBITMQ_URL')
 REDIS_URL = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}'
 UPLOAD_PATH = '/uploads'
 FILE_DURATION = int(os.environ.get('FILE_DURATION'))
